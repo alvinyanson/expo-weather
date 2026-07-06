c3cabb48a86f0459677ed6c554c2e4d9f58274c5
alvinyanson <a.yanson@arcanys.com>
2026-07-06 14:34:41 +0800

    triggger eas update demo

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -48,7 +48,7 @@ export const SearchHeader = () => {
           />
           <TextInput
             style={styles.searchInput}
-            placeholder="Search city..."
+            placeholder="Search city"
             placeholderTextColor={theme.colors.textHint}
             value={searchQuery}
             onChangeText={setSearchQuery}
